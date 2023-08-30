9e9175df65c10a7ecfa9e09374b9412c086d172b
Ricardo <ricardoparedesg16@gmail.com>
2023-08-29 19:54:40 -0600

    Muestra la informacion hasta abajo del producto

--- e_commerce/src/components/Products/Products.jsx
+++ e_commerce/src/components/Products/Products.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import './Products.css'; // Asegúrate de tener un archivo Products.css en la misma ubicación
+import './Products.css';
+import ProductDetail from '../ProductsDetail/ProductsDetail';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3000/items')
@@ -10,18 +12,23 @@ function Products() {
       .then(data => setProducts(data));
   }, []);
 
+  const handleProductClick = product => {
+    setSelectedProduct(product);
+  };
+
   return (
     <div>
       <h1>Productos</h1>
       <div className="product-container">
         {products.map(product => (
-          <div className="product-card" key={product.id}>
+          <div className="product-card" key={product.id} onClick={() => handleProductClick(product)}>
             <h2>{product.product_name}</h2>
             <img src={product.image} alt={product.product_name} />
             <p>$ {product.price}</p>
           </div>
         ))}
       </div>
+      {selectedProduct && <ProductDetail product={selectedProduct} />} {/* Mostrar los detalles */}
     </div>
   );
 }
---
+++ e_commerce/src/components/ProductsDetail/ProductsDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './productsdetail.css';
+
+function ProductDetail({ product }) {
+  return (
+    <div className="product-detail">
+      <h2>{product.product_name}</h2>
+      <img src={product.image} alt={product.product_name} />
+      <p>{product.description}</p>
+      <p>$ {product.price}</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
