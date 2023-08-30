064d2662f9010ca15f8b553f889b00258eb6c2e8
Ricardo <ricardoparedesg16@gmail.com>
2023-08-29 20:25:13 -0600

    Modificacion de popuo para que sea mas estetico

--- e_commerce/src/components/ProductsDetail/ProductsDetail.jsx
+++ e_commerce/src/components/ProductsDetail/ProductsDetail.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './productsdetail.css';
- 
+
 function ProductDetail({ product }) {
   return (
     <div className="product-detail">
+    <img src={product.image} alt={product.product_name} />
+    <div className="product-detail-content">
       <h2>{product.product_name}</h2>
-      <img src={product.image} alt={product.product_name} />
-      <p>{product.description}</p>
-      <p>$ {product.price}</p>
+      <div>
+        <p>{product.description}</p>
+        <p>$ {product.price}</p>
+      </div>
     </div>
+  </div>
   );
 }
 
