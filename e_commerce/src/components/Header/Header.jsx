3a2de7b63c674764e22597bc6aa9f4c62e11935b
Ricardo <ricardoparedesg16@gmail.com>
2023-08-31 12:43:11 -0600

    Agego select y  estilos, css de  products

--- e_commerce/src/components/Header/Header.jsx
+++ e_commerce/src/components/Header/Header.jsx
@@ -19,6 +19,34 @@ const Header = ({ onSearch }) => {
                 value={searchQuery}
                 onChange={handleSearchChange}
             />
+
+            <div className='header__filter-container'>
+                <select>
+                    <option value="">All Categories</option>
+                    <option value="Grocery">Grocery</option>
+                    <option value="Music">Music</option>
+                    <option value="Computers">Computers</option>
+                    <option value="Toys">Toys</option>
+                    <option value="Health">Health</option>
+                    <option value="">Kids</option>
+                    <option value="Grocery">Sports</option>
+                    <option value="Music">Outdoors</option>
+                    <option value="Computers">Shoes</option>
+                    <option value="Toys">Garden</option>
+                    <option value="Health">Games</option>
+                    <option value="Grocery">Jewelery</option>
+                    <option value="Music">Movies</option>
+                    <option value="Computers">Tools</option>
+                    <option value="Toys">Automotive</option>
+                    <option value="Health">Industrial</option>
+                    <option value="Grocery">Baby</option>
+                    <option value="Music">Beauty</option>
+                    <option value="Computers">Home</option>
+                    <option value="Toys">Electronics</option>
+                    <option value="Health">Books</option>
+                </select>
+            </div>
+
             <div className='header__li-container'>
                 <ul>
                     <li className='header__li'>
